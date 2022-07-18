bbf3d4b38532f80186ec06cf87552a7b4e9354d3
nalarza <103529597+nalarza@users.noreply.github.com>
2022-07-18 16:24:31 -0500

    Nelson
    añado los servicios que faltaban y la clase controladora pero no el contenido como tal "queda pendiente terminar"

---
+++ src/main/java/Torneo/Futbol/Servicio/PaisService.java
@@ -0,0 +1,21 @@
+package Torneo.Futbol.Servicio;
+
+import Torneo.Futbol.Modelo.Pais;
+import Torneo.Futbol.Repositorio.PaisRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class PaisService {
+    @Autowired
+    PaisRepositorio paisRepositorio;
+    public List<Pais> listarPais(){
+        return (List<Pais>) paisRepositorio.findAll();
+    }
+    public Pais añadirPais(@RequestBody Pais pais){
+        return paisRepositorio.save(pais);
+    }
+}
